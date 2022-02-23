fee8f037e4549c990bbb00083bd1bf3539639043
iwashiroK <65843183+iwashiroK@users.noreply.github.com>
2022-02-23 15:27:59 +0900

    ランダムで話題を出力するように改修

--- front/screen.js
+++ front/screen.js
@@ -47,28 +47,66 @@ window.onload = $(function(){
   // 　　console.log("errorThrown : " + errorThrown.message);
   //   });
 
-  //話題を取得する
-  $.ajax({
-    url: "http://localhost/back/screen.php",
-    type: "GET",
-    //data: {
-      //"random_val":random,
-    //},
-    dataType : "json",
-    timespan:1000
-  }).done(function(data){
-    console.log(data);
-    topic_array = data;
-    data_max_count = data.length;
-    console.log(topic_array);
-    console.log(data_max_count);
-  }).fail(function(XMLHttpRequest, textStatus, errorThrown){
-    console.log("XMLHttpRequest : " + XMLHttpRequest.status);
-    console.log("textStatus : " + textStatus);
-    console.log("errorThrown : " + errorThrown.message);
-  });
+  
+  //setTimeout(createRandom(data_max_count), 5000);
 
-  setTimeout(createRandom(data_max_count), 5000);
+  //ajax関数のリターン値としてDeferredオブジェクトを受け取る
+  var deferred = ajax_function();
+
+  deferred.promise().then(function(){
+    console.log("ランダム"+Date.now());
+    createRandom(data_max_count-1);
+    console.log(randoms);
+  });
+  
+  
+  //次へボタンクリック
+  $('#btn').click(function(){
+    
+    //格納された配列の乱数のお題を取得する
+    $('.odai_in').html(topic_array[randoms[btn_count]]);
+
+    //ボタンをクリックした回数
+    btn_count++;
+    
+    //random = randoms[i];
+  });
+  
+  //ajax通信を行う
+  function ajax_function(){
+    console.log('ajax start');
+    //完了を知らせるためにDeferredオブジェクトを生成しそれを返す
+    var deferred = new $.Deferred();
+
+    //話題を取得する
+    $.ajax({
+      url: "http://localhost/back/screen.php",
+      type: "GET",
+      //data: {
+        //"random_val":random,
+      //},
+      dataType : "json",
+      timespan:1000
+    }).done(function(data){
+      console.log(data);
+      topic_array = data;
+      data_max_count = data.length;
+      console.log(topic_array);
+      console.log(data_max_count);
+      console.log("ajax done"+Date.now());
+    }).fail(function(XMLHttpRequest, textStatus, errorThrown){
+      console.log("XMLHttpRequest : " + XMLHttpRequest.status);
+      console.log("textStatus : " + textStatus);
+      console.log("errorThrown : " + errorThrown.message);
+    }).always(function(){
+      console.log('ajax finish');
+      console.log("ajax fin"+Date.now());
+      //ajax処理を終了したことをDeferredオブジェクトに通知
+      deferred.resolve();
+    });
+    //完了を知らせるためにDeferredオブジェクトを生成しそれを返す
+    return deferred;
+  }
 
   //重複しない乱数作成
   function createRandom(data_max_count){
@@ -82,22 +120,9 @@ window.onload = $(function(){
       }
     }
   }
-
+  //minからmaxの間での乱数作成
   function intRandom(min, max){
     return Math.floor( Math.random() * (max - min + 1)) + min;
   }
-  console.log(randoms);
-
-  //次へボタンクリック
-  $('#btn').click(function(){
-    //ボタンをクリックした回数
-    btn_count++;
-    
-    $('.odai_in').html(topic_array[btn_count-1]);
-    
-    //random = randoms[i];
-  });
-    
-
 
 });
