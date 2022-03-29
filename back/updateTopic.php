26b4dc99924fb6e0235cea3559c01576ac22a3fc
iwashiroK <65843183+iwashiroK@users.noreply.github.com>
2022-03-29 18:05:54 +0900

    close #7 db接続情報を別ファイルから読み込むように修正

--- back/updateTopic.php
+++ back/updateTopic.php
@@ -1,25 +1,10 @@
 <?php
-  $pdo_dsn = 'mysql:host=127.0.0.1;dbname=topic_db_20220129;charset=utf8mb4';
-  $pdo_user = 'root';
-  $pdo_pass = 'sopPy6020';
-  $pdo_option = array(
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES => false,
-    PDO::ATTR_STRINGIFY_FETCHES => false
-  );	
-  
-  try {
-    $pdo = new PDO($pdo_dsn, $pdo_user, $pdo_pass, $pdo_option);
-  } catch (Exception $e) {
-    header('Content-Type: text/plain; charset=UTF-8', true, 500);
-    exit($e->getMessage());
-    echo $e->getMessage();
-    echo "接続失敗";
-  }
-
+  header("Access-Control-Allow-Origin: *");
   
   try{
+    //db接続ファイルを読み込む
+    require_once dirname(__FILE__) . '/dbinfo/dbConnect.php';
+
     $query = '';
     $update_topic = [];
     $query_count = 0;
@@ -31,8 +16,7 @@
     );
     //echo json_encode($res, JSON_UNESCAPED_UNICODE);
 
-    
-    header("Access-Control-Allow-Origin: *");
+    header('Content-Type: application/json; charset=UTF-8');
     //フロントからチェックされたカテゴリーを取得する
     $update_topic = $_POST;
     //echo json_encode($update_topic['category_id'], JSON_UNESCAPED_UNICODE);
